a3e3e4211b1a031ec3faced8b63c4da70ae7286d
yuri-almir-pinto <yurialmirp@gmail.com>
2025-02-12 20:47:27 -0300

    Recarregar arquivo .gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBdij-6f3E6LnJOPBdaQiQbpwzLURPqAfs",
-    authDomain: "coin-hunters-club.firebaseapp.com",
-    projectId: "coin-hunters-club",
-    storageBucket: "coin-hunters-club.firebasestorage.app",
-    messagingSenderId: "987543843897",
-    appId: "1:987543843897:web:b3224469ac68ea479709e8",
-    measurementId: "G-JNJ1681P3E"
-};
